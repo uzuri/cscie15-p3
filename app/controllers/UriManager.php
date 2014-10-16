92374848cd0a8f200e549ac99c7dfd3eb8de87fd
Rebecca Mazur <mazurr@rebeccas-mbp.home>
2014-10-16 18:18:18 -0400

    Once again we have things not working on production; dump  to figure out what's going on

--- app/controllers/UriManager.php
+++ app/controllers/UriManager.php
@@ -10,7 +10,7 @@ class UriManager
 	
 	function __construct() 
 	{
-		print_r($_SERVER);
+		print_r($_SERVER['HTTP_X_FORWARDED_HOST']);
 		if (isset($_SERVER['HTTP_X_FORWARDED_HOST']))
 		{
 			$uri = "http://" . $_SERVER['HTTP_X_FORWARDED_HOST'];
